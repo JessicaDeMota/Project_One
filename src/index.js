e4f43ec600b91d61dee0cc65e854836357f71c8e
Jessica De Mota <jessicamunozz601@gmail.com>
2023-03-08 14:31:48 -0500

    commiting changes to fix react dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './fonts/CarnivalmfOpenshadow-ld5w.ttf'
 import './index.css';
 import App from './App';
@@ -8,16 +9,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<h1>Your App</h1>)
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<h1>Project_One</h1>)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
+reportWebVitals();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
